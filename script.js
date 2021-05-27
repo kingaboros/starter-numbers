ac2429c1e2938d0757dd21eb8884cafe4a6c6575
kingaboros <71248858+kingaboros@users.noreply.github.com>
2021-05-27 20:54:32 +0300

    Numbers 1

--- script.js
+++ script.js
@@ -251,3 +251,174 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+/*
+//////// Converting and checking numbers //////////
+
+console.log(23 === 23.0);
+
+console.log(0.1 + 0.2); //JS can't represent certain fractions
+console.log(0.1 + 0.2 === 0.3); //this should be true, but because of the error in JS is false
+
+// Converting from string to numbers
+console.log(Number('23'));
+console.log(+'23'); // simpler way to convert a string to a number. JS does type coercion when seeing the + op.
+
+// Parsing for intergers
+
+console.log(Number.parseInt('30px', 10));
+console.log(Number.parseInt('p2', 10)); // NaN - it must
+
+// Parse float
+console.log(Number.parseFloat('2.5rem')); // displays the decimal as well
+
+console.log(Number.parseInt('2.5rem')); // stops at 2
+
+// Check is a value NaN
+console.log(Number.isNaN(20));
+console.log(Number.isNaN('20'));
+console.log(Number.isNaN(+'20x'));
+console.log(Number.isNaN(23 / 0));
+
+// Check is a value is a number
+console.log(Number.isFinite(20));
+console.log(Number.isFinite('20'));
+console.log(Number.isFinite(+'20x'));
+console.log(Number.isFinite(23 / 0));
+
+// Check is a value is integer
+console.log(Number.isInteger(20));
+console.log(Number.isInteger(20.0));
+console.log(Number.isInteger(23 / 0));
+
+
+
+/////////////// Math and rounding ///////////////////
+
+// Square route
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2)); // calculate the square route
+console.log(8 ** (1 / 3)); // this is how to calculate a cubic route
+
+// return the max value
+
+console.log(Math.max(23, 435, 6, 77, 2));
+console.log(Math.max(23, '435', 6, 77, 2)); // type coercion
+console.log(Math.max(23, '435px', 6, 77, 2)); // parsing doesn't work
+
+// return the min value
+
+console.log(Math.min(23, 435, 6, 77, 2));
+
+//constant - calculate the radious of a circle
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+// generating a random number
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+// random number function
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
+// 0 ...1 -> 0 ... (max-min) -> min...max
+
+// console.log(randomInt(10, 20));
+
+// Rounding integers
+
+console.log(Math.trunc(23.5)); // removes decimals
+
+console.log(Math.round(23.3)); // rounds to the nearest integer
+console.log(Math.round(23.9));
+
+console.log(Math.ceil(23.3)); // rounds up
+console.log(Math.ceil(23.9));
+
+console.log(Math.floor(23.3)); // rounds down
+console.log(Math.floor(23.9));
+
+console.log(Math.trunc(-23.3)); // just removes the decimal
+console.log(Math.floor(-23.9)); // with negative numbers it;s the other way arround
+
+// Rounding floating point numbers (decimals)
+
+console.log((2.7).toFixed(0)); // tofixed always returns a string
+console.log((2.7).toFixed(3)); // adds zeros until it has 3 decimal parts
+console.log((2.345).toFixed(2));
+console.log(+(2.345).toFixed(2)); // converts a string to a number
+
+
+////////////// Remainder operator % ////////////////
+
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2*2+1
+
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2*3 + 2
+
+console.log(6 % 2);
+console.log(6 / 2); // 6 = 2*3 + 0
+
+console.log(7 % 2);
+console.log(7 / 2); // 7 = 2*3 + 1
+
+// Check if a number is divizible to any number
+const isEven = n => n % 2 === 0;
+
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+
+/////////// Working with bigInt ////////
+
+console.log(2 ** 53 - 1); // the biggest number JS can hold
+
+console.log(Number.MAX_SAFE_INTEGER);
+
+console.log(2 ** 53 + 1); // unsafe numbers -> if we add other numbers, JS will not represent these numbers accurately - we might loose precision
+
+console.log(598649864532651794587149659865396539865); // can't display it to the console
+console.log(598649864532651794587149659865396539865n);
+
+console.log(BigInt(75630263276116));
+
+// Operations
+
+console.log(100000n + 100000n);
+console.log(76423656326255n * 75639876628762972n);
+
+// console.log(Math.sqrt(16n)); // it's not going to work
+
+const huge = 26453287653927591875198651n;
+const num = 23;
+console.log(huge * BigInt(num)); //
+
+// Exceptions
+
+console.log(20n > 15);
+console.log(20n === 20);
+console.log(typeof 20n);
+console.log(20n == 20);
+
+console.log(huge + 'is REALLY big!!!');
+
+// Divisions
+console.log(11n / 3n);
+console.log(10 / 3);
+
+*/
+
+/////// Creating dates //////////
+
+// How to create a date
+
+const now = new Date();
+console.log(now);
+
+console.log(new Date('May 27 2021 16:45:58 GMT+0300'));
+console.log(new Date('December 24, 2015'));
+
+console.log(new Date(account1.movementsDates[0]));
+console.log(new Date(2037, 10, 19, 15, 23, 5));
